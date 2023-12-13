9c3515e97d7b74104913b241a2aa31f4bad79042
Giorgi Giorgobiani <Giorgigiorgobiani@hotmail.com>
2023-12-13 11:46:54 -0500

    added create user handler

--- src/handlers/createUserHandler.ts
+++ src/handlers/createUserHandler.ts
@@ -1,23 +1,128 @@
+import {
+    validateEmail,
+    validateUsername,
+    validatePassword,
+} from '../utils/utils';
+import Logger from '../logger/Logger';
+import { v4 as uuidv4 } from 'uuid';
+import bcrypt from 'bcrypt';
 import { Request, Response } from 'express';
-import { createUser } from '../sqlCalls/userService/userService';
+import UserService from '../sqlCalls/userService/userService';
 
 
-const createUserHandler = async (req: Request, res: Response) => {
+const createUserHandler = async (req: Request, res: Response): Promise<void> => {
+
+
     const { name, email, phone, password } = req.body;
 
     // Validate that name, email, and phone are provided in the request body
     if (!name || !email || !phone) {
-        return res.status(400).json({ error: 'Invalid input' });
+
+        Logger.error(`Invalid input: name: ${name}, email: ${email}, phone: ${phone}\n`);
+        res.status(400).json({ error: 'Invalid input' });
+
     }
 
-
-    try {
-        const user = await createUser(name, email, phone, password);
-        res.status(200).json({ result: "success", user: user[0], message: "User created successfully" });
-    } catch (error) {
-        console.error('Error creating applicant:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+    //Validate the user data
+    Logger.info(`Validating the email\n`);
+    const emailValidated: boolean = validateEmail(email);
+    if (!emailValidated) {
+        Logger.error(`Invalid email\n`);
+        res.status(400).send({ message: 'Invalid email' });
+        return;
     }
+
+    Logger.info(`Validating the username\n`);
+    const usernameValidated: boolean = validateUsername(name);
+    if (!usernameValidated) {
+        Logger.error(`Invalid username\n`);
+        res.status(400).send({ message: 'Invalid username' });
+        return;
+    }
+
+    Logger.info(`Validating the password\n`);
+    const passwordValidated: boolean = validatePassword(password);
+    if (!passwordValidated) {
+        Logger.error(`Invalid password\n`);
+        res.status(400).send({ message: 'Invalid password, must be 8 chars or longer' });
+        return;
+    }
+
+    //Check if user already exists
+    Logger.info(`Checking if user already exists\n`);
+    await UserService.getUserByUsername(name).then((user: any) => {
+
+        if (user.length > 0) {
+
+            Logger.error(`User already exists\n`);
+            res.status(400).send({ message: 'User already exists' });
+            return;
+
+        } else {
+
+            createNewUser(res, name, email, phone, password);
+
+        }
+
+    }).catch((error: any) => {
+
+        Logger.error(`Error checking if user already exists: ${error}\n`);
+        res.status(500).send({ message: 'Error checking if user already exists' });
+        return;
+
+    });
 };
+
+//Create a new user
+const createNewUser = async (res: Response, name: string, email: string, phone: string, password: string): Promise<any> => {
+
+    //Generate UUID
+    const id: string = uuidv4();
+
+    //Hash the password
+    const hashedPassword: string = await bcrypt.hash(password, 10);
+
+    //Create the user
+    const user: any = await UserService.createUser(name, email, phone, hashedPassword, id).then((user: any) => {
+
+        if (user.length > 0) {
+
+            Logger.info(`User created successfully\n`);
+
+            //Get the user from the response
+            const newUser: any = user[0];
+
+            //Send the user in the response
+            console.log(`Sending the user in the response\n`);
+            const response: Record<string, any> = {
+                result: "success",
+                user: newUser,
+                message: 'User created',
+                statusCode: 200,
+            };
+
+
+            res.status(200).send(response);
+            return;
+
+        } else {
+
+            Logger.error(`Error creating user\n`);
+            res.status(500).send({ message: 'Error creating user' });
+            return;
+        }
+
+
+    }).catch((error: any) => {
+
+        res.status(500).send({ message: 'Error creating user' });
+        Logger.error(`Error creating user: ${error}\n`);
+        return error;
+
+    });
+
+    return user;
+
+}
 
 export default createUserHandler;
--- src/sqlCalls/userService/userService.ts
+++ src/sqlCalls/userService/userService.ts
@@ -1,78 +1,107 @@
+import exp from 'constants';
 import { pool } from '../db';
 
-// SQL call to get user by id
-export const getUser = async (id: string): Promise<any> => {
 
-    try {
+// User Service Class
+class UserService {
 
-        const { rows } = await pool.query(
-            'SELECT * FROM users WHERE id = $1',
-            [id]
-        );
-        return rows;
+    // SQL call to get user by id
+    async getUser(id: string): Promise<any> {
 
-    } catch (error) {
+        try {
 
-        console.error('Error getting user:', error);
-        return error;
+            const { rows } = await pool.query(
+                'SELECT * FROM users WHERE id = $1',
+                [id]
+            );
+            return rows;
 
+        } catch (error) {
+
+            console.error('Error getting user:', error);
+            return error;
+
+        }
+
+    };
+
+    //Get User by name
+    async getUserByUsername(name: string): Promise<any> {
+
+        try {
+
+            const { rows } = await pool.query(
+                'SELECT * FROM users WHERE name = $1',
+                [name]
+            );
+            return rows;
+
+        } catch (error) {
+
+            console.error('Error getting user:', error);
+            return error;
+
+        }
+
+    };
+
+    // SQL call to create user with name, email, phone, and password
+    async createUser(name: string, email: string, phone: string, password: string, id: string): Promise<any> {
+
+        try {
+
+            const { rows } = await pool.query(
+                'INSERT INTO users (name, email, phone, password, uuid) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+                [name, email, phone, password, id]
+            );
+            return rows;
+
+        } catch (error) {
+
+            console.error('Error creating user:', error);
+            return error;
+
+        }
     }
 
-};
+    // SQL call to delete user by id
+    async deleteUser(id: string): Promise<any> {
 
-// SQL call to create user with name, email, phone, and password
-export const createUser = async (name: string, email: string, phone: string, password: string): Promise<any> => {
+        try {
 
-    try {
+            const { rows } = await pool.query(
+                'DELETE FROM users WHERE uuid = $1',
+                [id]
+            );
+            return rows;
 
-        const { rows } = await pool.query(
-            'INSERT INTO users (name, email, phone, password) VALUES ($1, $2, $3, $4) RETURNING *',
-            [name, email, phone, password]
-        );
-        return rows;
+        } catch (error) {
 
-    } catch (error) {
+            console.error('Error deleting user:', error);
+            return error;
 
-        console.error('Error creating user:', error);
-        return error;
-
+        }
     }
+
+    // SQL call to update user by id
+    async updateUser(id: string, name: string, email: string, phone: string, password: string): Promise<any> {
+
+        try {
+
+            const { rows } = await pool.query(
+                'UPDATE users SET name = $1, email = $2, phone = $3, password = $4 WHERE id = $5 RETURNING *',
+                [name, email, phone, password, id]
+            );
+            return rows;
+
+        } catch (error) {
+
+            console.error('Error updating user:', error);
+            return error;
+
+        }
+    }
+
 }
 
-// SQL call to delete user by id
-export const deleteUser = async (id: string): Promise<any> => {
-
-    try {
-
-        const { rows } = await pool.query(
-            'DELETE FROM users WHERE id = $1',
-            [id]
-        );
-        return rows;
-
-    } catch (error) {
-
-        console.error('Error deleting user:', error);
-        return error;
-
-    }
-}
-
-// SQL call to update user by id
-export const updateUser = async (id: string, name: string, email: string, phone: string, password: string): Promise<any> => {
-
-    try {
-
-        const { rows } = await pool.query(
-            'UPDATE users SET name = $1, email = $2, phone = $3, password = $4 WHERE id = $5 RETURNING *',
-            [name, email, phone, password, id]
-        );
-        return rows;
-
-    } catch (error) {
-
-        console.error('Error updating user:', error);
-        return error;
-
-    }
-}
+export default new UserService();
